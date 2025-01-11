fc8039e26fec986ddefabe841a18c7bd49495d12
yasmiinie <ly_zaidi@esi.dz>
2025-01-11 09:32:51 +0100

    starting the merge

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,4 +81,7 @@ dependencies {
     implementation(libs.google.play.services.location)
     implementation(libs.kotlinx.coroutines.play.services)
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 }
